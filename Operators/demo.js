ac542694c5670e4b342a8131fc13052c66314ce2
Rashini1998 <gamalathrs.19@uom.lk>
2024-06-20 16:50:36 +0530

    operators

---
+++ Operators/demo.js
@@ -0,0 +1,16 @@
+function myFunc() {
+  let x = 5;
+  let y = 10;
+  let z = x + y;
+  console.log(z);
+}
+
+function demo() {
+  let x = 10;
+  x **= 5;
+  console.log(x);
+}
+
+demo();
+
+// myFunc();
